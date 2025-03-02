343a998e841c4a9c4737c4764b37d71a766d80d9
hxsggsz <vt.hugo.2021@gmail.com>
2025-03-02 19:30:54 -0300

    refactor: add position on clientside to avoid every node jumping on new request

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import {
   ReactFlow,
   Background,
-  MiniMap,
   useNodesState,
   useEdgesState,
   Edge,
@@ -30,17 +29,33 @@ export default function App() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
     []
   );
+
   useEffect(() => {
     if (!diagram || !diagram.data) return;
-    setNodes(diagram.data);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [diagram?.data]);
+
+    const newNodes = diagram.data.map((diagramNode) => {
+      const existingNode = nodes.find((node) => node.id === diagramNode.id);
+
+      if (existingNode) {
+        return existingNode;
+      }
+
+      return {
+        ...diagramNode,
+        position: {
+          x: Math.random() * 400,
+          y: Math.random() * 400,
+        },
+      };
+    });
+
+    setNodes(newNodes);
+  }, [diagram, diagram?.data, nodes, setNodes]);
 
   useEffect(() => {
     if (!diagram || !diagram.relations) return;
     setEdges(diagram.relations);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [diagram?.relations]);
+  }, [diagram, diagram?.relations, setEdges]);
 
   return (
     <div className={css.wrapper}>
@@ -57,7 +72,6 @@ export default function App() {
         fitView
       >
         <Background />
-        <MiniMap />
       </ReactFlow>
     </div>
   );
