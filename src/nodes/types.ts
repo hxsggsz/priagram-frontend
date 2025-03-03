2fd5bffa993f17ab64f9a4fd74c124c1fc7e3d99
hxsggsz <vt.hugo.2021@gmail.com>
2025-03-03 11:16:19 -0300

    styles: add reset css

--- src/nodes/types.ts
+++
@@ -1,5 +0,0 @@
-import { PrismaNode } from "@/types/diagram";
-import type { Node, BuiltInNode } from "@xyflow/react";
-
-export type PositionLoggerNode = Node<{ label: string }, "position-logger">;
-export type AppNode = BuiltInNode | PositionLoggerNode | PrismaNode;
